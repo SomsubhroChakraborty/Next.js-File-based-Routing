cdf2b9f70b9d0a4319f3c1c5f7758a5e3fd10453
Somsubhro Chakraborty <140301396+SomsubhroChakraborty@users.noreply.github.com>
2024-04-20 20:27:05 +0530

    Add files via upload
    In this repo the discussion is on file based routing in Next.js

---
+++ app/About/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1 className="text-xl">Problems solved by Next.js</h1>
+        <ul>
+            <li>Full stack solution</li>
+            <li>File based Routing</li>
+            <li>Optimized Rendering</li>
+        </ul>
+    </div>
+  )
+}
+
+export default About 
---
+++ app/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Components = () => {
+  return (
+    <div>
+        I am navbar.
+    </div>
+  )
+}
+
+export default Components
